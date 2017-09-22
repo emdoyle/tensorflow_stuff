aa865790e996a28d83223012e721719786b4ec86
Evan M Doyle <evanmdoyle@gmail.com>
2017-09-22 19:01:40 +0200

    Step by step creation of deep NN in Jupyter notebook without TF

--- Non-Tensorflow/math_utils.py
+++ Non-Tensorflow/math_utils.py
@@ -9,6 +9,12 @@ def L2_reg(lambda_, w1, w2):
 def L1_reg(lambda_, w1, w2):
     return (lambda_ / 2.0) * (np.abs(w1).sum() + np.abs(w2).sum())
 
+def L1_reg_deep(lambda_, weights):
+	return (lambda_ / len(weights)) * np.sum(list(map(lambda x: np.abs(x).sum(), weights)))
+
+def L2_reg_deep(lambda_, weights):
+	return (lambda_ / len(weights)) * np.sum(list(map(lambda x: np.sum(x[:, 1:] ** 2), weights)))
+
 
 def cross_entropy(output, y_target):
     return - np.sum(np.log(output) * (y_target), axis=1)
